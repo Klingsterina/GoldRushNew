d71ed035d194cc4acb1c03fcd79286858c69d07e
Klingsterina <eyglo2001@gmail.com>
2026-04-22 14:53:35 +0000

    more tests

--- src/test/java/vinnsla/LeikurTest.java
+++ src/test/java/vinnsla/LeikurTest.java
@@ -7,8 +7,48 @@ import static org.junit.jupiter.api.Assertions.*;
 class LeikurTest {
 
     @Test
-    void testLeikurIsCreated() {
+    void leikurStartsWithDefaultDifficultyZero() {
         Leikur leikur = new Leikur();
-        assertNotNull(leikur);
+
+        assertEquals(0, leikur.getErfidleikastig());
+    }
+
+    @Test
+    void setErfidleikastigUpdatesDifficulty() {
+        Leikur leikur = new Leikur();
+
+        leikur.setErfidleikastig(2);
+
+        assertEquals(2, leikur.getErfidleikastig());
+    }
+
+    @Test
+    void leikurHasExpectedTimes() {
+        Leikur leikur = new Leikur();
+
+        assertArrayEquals(new int[]{60, 45, 30}, leikur.getTimar());
+    }
+
+    @Test
+    void leikurCreatesKlukka() {
+        Leikur leikur = new Leikur();
+
+        assertNotNull(leikur.getKlukka());
+        assertEquals(0, leikur.getKlukka().getTimi());
+    }
+
+    @Test
+    void leikurCreatesTwoPlayers() {
+        Leikur leikur = new Leikur();
+
+        assertNotNull(leikur.getSpilari1());
+        assertNotNull(leikur.getSpilari2());
+    }
+
+    @Test
+    void tveirSpilararIsFalseByDefault() {
+        Leikur leikur = new Leikur();
+
+        assertFalse(leikur.isTveirSpilarar());
     }
 }
--- src/test/java/vinnsla/SpilariTest.java
+++ src/test/java/vinnsla/SpilariTest.java
@@ -7,8 +7,27 @@ import static org.junit.jupiter.api.Assertions.*;
 class SpilariTest {
 
     @Test
-    void testSpilariIsCreated() {
+    void spilariStartsWithZeroScore() {
         Spilari spilari = new Spilari();
-        assertNotNull(spilari);
+
+        assertEquals(0, spilari.getStig());
+    }
+
+    @Test
+    void setStigUpdatesScore() {
+        Spilari spilari = new Spilari();
+
+        spilari.setStig(15);
+
+        assertEquals(15, spilari.getStig());
+    }
+
+    @Test
+    void stigPropertyMatchesScore() {
+        Spilari spilari = new Spilari();
+
+        spilari.setStig(42);
+
+        assertEquals(42, spilari.stigProperty().get());
     }
 }
